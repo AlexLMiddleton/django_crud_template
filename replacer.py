6d41e8c05a00adbe8b2e3a451218543a1beea2d1
Alexander Middleton <alexander_middleton@charleston.k12.sc.us>
2020-02-21 15:06:29 -0500

    Added comments to the replacer.py file.

--- replacer.py
+++ replacer.py
@@ -27,11 +27,14 @@ models_number = int(models_input)
 #########################################
 
 # Functions
+
+# Loop through folders searching for files that start with a particular string, and then replace it with a user-defined string
 def rename_folder(old_folder_name, replaced_name):
     for entry in os.scandir('./'):
         if entry.name.startswith(old_folder_name):
             os.rename(os.path.join('./', entry.name), os.path.join('./', entry.name.replace(old_folder_name, replaced_name.lower())))
 
+# Scan all files in a given folder, searching for a particular hardcoded string, and replace it with a user-defined string
 def scan_files_and_replace(folder_name, old_file_name, replaced_file_name):
     for entry in os.scandir(f'./{folder_name}'):
         if entry.name.endswith('.py'):
@@ -86,15 +89,19 @@ scan_files_and_replace(app_name, 'django_template_app', app_name)
 os.mkdir(f'./{app_name}/templates/{app_name}')
 
 for x in range(models_number):
+    # Ask the user for the name for each of the models, which will then be used to build Models, URLs, Forms, Views, HTML templates, etc.
     model_name = input('Enter the name for model ' + str(x + 1) + ': ')
+    # Build a barebones model template for each model name they enter (comes with ID primary key and a generic CharField)
     with open(f'./{app_name}/models.py', 'a+') as modelFile:
         modelFile.write(f'{nl}class {model_name}(models.Model):{nl}{tab}id = models.AutoField(primary_key=True){nl}{tab}{model_name.lower()}_name = models.CharField(max_length = 30)')
+    # Create the views for each model
     with open(f'./{app_name}/views.py', 'a+') as viewFile:
         viewFile.write(f'{nl}{nl}class {model_name}ListView(ListView):{nl}{tab}model={model_name}')
         viewFile.write(f'{nl}{nl}class {model_name}DetailView(DetailView):{nl}{tab}model={model_name}{nl}{tab}template_name=\'{app_name}/{model_name.lower()}_detail.html\'')
         viewFile.write(f'{nl}{nl}class {model_name}CreateView(CreateView):{nl}{tab}model={model_name}{nl}{tab}template_name=\'{app_name}/{model_name.lower()}_add.html\'{nl}{tab}fields=\'__all__\'{nl}{tab}success_url=reverse_lazy(\'' + model_name.lower() + '-list\')')
         viewFile.write(f'{nl}{nl}class {model_name}UpdateView(UpdateView):{nl}{tab}model={model_name}{nl}{tab}template_name=\'{app_name}/{model_name.lower()}_edit.html\'{nl}{tab}fields=\'__all__\'{nl}{tab}success_url=reverse_lazy(\'' + model_name.lower() + '-list\')')
         viewFile.write(f'{nl}{nl}class {model_name}DeleteView(DeleteView):{nl}{tab}model={model_name}{nl}{tab}template_name=\'{app_name}/{model_name.lower()}_delete.html\'{nl}{tab}success_url=reverse_lazy(\'{model_name.lower()}-list\')')
+    # Create the URL patterns for each model
     with open(f'./{app_name}/urls.py', 'a') as urls:
         urls.seek(urls.tell() - 1, os.SEEK_SET)
         urls.truncate()
@@ -104,15 +111,19 @@ for x in range(models_number):
         urls.write(f'{nl}{tab}path(\'{model_name.lower()}/edit/<int:pk>/\', {model_name}UpdateView.as_view(), name=\'{model_name.lower()}-edit\'),')
         urls.write(f'{nl}{tab}path(\'{model_name.lower()}/delete/<int:pk>/\', {model_name}DeleteView.as_view(), name=\'{model_name.lower()}-delete\'),{nl}')
         urls.write(']')
+    # Build out a form for each model
     with open(f'./{app_name}/forms.py', 'a') as forms:
         forms.write(f'{nl}class {model_name}Form(ModelForm):{nl}{tab}class Meta:{nl}{tab}{tab}model = {model_name}{nl}{tab}{tab}exclude = []{nl}{tab}{tab}fields = \'__all__\'{nl}')
+    # Register each model in the admin file
     with open(f'./{app_name}/admin.py', 'a') as adminRegistry:
         adminRegistry.write(f'{nl}admin.site.register({model_name})')
+    # Copy the template HTML files, place them in the correct spot to be read, and then rename them to the {model}_<file_ending>.html format
     shutil.copyfile(f'./{app_name}/templates/index.html', f'./{app_name}/templates/{app_name}/{model_name}_list.html')
     shutil.copyfile(f'./{app_name}/templates/newmodel_add.html', f'./{app_name}/templates/{app_name}/{model_name}_add.html')
     shutil.copyfile(f'./{app_name}/templates/newmodel_delete.html', f'./{app_name}/templates/{app_name}/{model_name}_delete.html')
     shutil.copyfile(f'./{app_name}/templates/newmodel_detail.html', f'./{app_name}/templates/{app_name}/{model_name}_detail.html')
     shutil.copyfile(f'./{app_name}/templates/newmodel_edit.html', f'./{app_name}/templates/{app_name}/{model_name}_edit.html')
+    # Scan the templates directory.  When you find an instance of "newmodel", "ModelTitle", or "ProjectTitle", replace them with the appropriate user-input values
     for entry in os.scandir(f'./{app_name}/templates/{app_name}'):
         with open(entry, 'r') as current:
             read_entry = current.read()
@@ -120,6 +131,7 @@ for x in range(models_number):
         with open(entry, 'w') as current:
             current.write(replace_text)
 
+# Do a precautionary migrate, then make migrations, and then push those migrations up
 os.system('python manage.py migrate')
 os.system('python manage.py makemigrations')
 os.system('python manage.py migrate')
